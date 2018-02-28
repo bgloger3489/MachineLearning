5a65409209aaaa9d04f6df0bf163043556c7d7de
BT_1N3_11 <BT_1N3_11@BT_1N3_11.bktech.local>
2018-02-28 11:24:41 -0500

    Need to finish work on derivitive math in Mult

--- ML/src/Nodes/Mult.java
+++ ML/src/Nodes/Mult.java
@@ -31,4 +31,35 @@ public class Mult extends Node{
 		
 		return Matrix.addMatrix(Matrix.multiplyMatricies(a.backprop(goal), b.matrix), Matrix.multiplyMatricies(a.matrix, b.backprop(goal)));
 	}
+	
+	public Matrix acutallBackprop(Tensor goal) {
+		//NEED TO FICURE OUT HOW IT WORKS IN M(B + X)!!!!!!
+		//Tensor respectTo = goal;
+		int NUMPICTURES = 0;
+		Tensor notGoal = null;
+		
+		if(a == goal)
+			b = notGoal;
+		else if(b == goal)
+			a = notGoal;
+		else
+			System.out.println("FIX BACKPROP IN MULT!!!");
+		
+		
+		//double[][] goalback = a.backprop(goal).vals;
+		double[][] notGoalBack = notGoal.backprop(goal).vals;
+		
+		double[][] emptyGrad = new double[goal.matrix.vals.length][goal.matrix.vals[0].length];
+		
+		for(int i = 0; i < emptyGrad.length; i++) {
+			for(int j = 0; j< emptyGrad[0].length; j++) {
+				for(int p = 0; p < NUMPICTURES; p++) {
+					emptyGrad[i][j]+= notGoal.matrix.vals[p][i]*goal.matrix.vals[i][j];
+				}
+			}
+		}	
+		
+		return new Matrix(emptyGrad);
+	}
+	
 }
